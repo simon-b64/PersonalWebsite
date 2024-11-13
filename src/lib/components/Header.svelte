17d69ef8368f9be6dcb1b20123332ce639e79d20
Simon B. <47189894+simon-b64@users.noreply.github.com>
2024-11-13 16:22:06 +0100

    feat: Added 404 error page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,2 +1,17 @@
 <script lang="ts">
+	import logo from '$lib/images/logo.svg';
 </script>
+
+<nav class="navbar container mx-auto flex items-center p-1">
+	<img src={logo} alt="Logo" class="logo" />
+</nav>
+
+<style>
+	.navbar {
+		height: 3rem;
+	}
+
+	.logo {
+		max-height: 2rem;
+	}
+</style>
