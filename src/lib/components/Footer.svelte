862125886d7c030d66b2704c2c2b9568adbf78f8
Simon B. <47189894+simon-b64@users.noreply.github.com>
2024-11-29 21:16:28 +0100

    feat: Removed tailwindcss and reworked hero section

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts"></script>
+
+<footer class="fixed left-0 top-0 h-12 w-full">
+	<div class="container mx-auto flex h-full items-center px-4">
+		<span class="text-xl tracking-[.5rem]">Simon B.</span>
+	</div>
+</footer>
