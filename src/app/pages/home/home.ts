91e40291f0d7e5b92e924392625d588ed7ff99f5
Simon B. <47189894+simon-b64@users.noreply.github.com>
2025-06-08 17:26:06 +0200

    feat: Added basic layout

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class HomePage {
+
+}
