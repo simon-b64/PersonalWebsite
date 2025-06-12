a321b735eb612b864a251024ad967af62b6799b6
Simon B. <47189894+simon-b64@users.noreply.github.com>
2025-06-12 11:12:58 +0200

    chore: Enabled angular image optimisation

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,12 +2,14 @@ import {Component} from '@angular/core';
 import {Logo} from '../../components/logo/logo';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { faAnglesDown, faLightbulb, faSearch, faUsers, faSmile } from '@fortawesome/free-solid-svg-icons';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
     imports: [
         Logo,
         FontAwesomeModule,
+        NgOptimizedImage,
     ],
   templateUrl: './home.html',
   styleUrl: './home.css'
